c5b483c8db5b1acbf7679917101b6cb272388512
Matheus Farias <mfarias@mmcafe.com.br>
2021-02-28 14:06:05 -0300

    Exercise 2 - seed 50 properties

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+if Property.count == 0
+    path = File.join(File.dirname(__FILE__), "./seeds/properties.json")
+    records = JSON.parse(File.read(path))
+    records.each do |record|
+        property = Property.create(name: record['name'])
+        photos = record['photos']
+        photos.each do |photo|
+            property.photos.create(name: photo['name'], url: photo['url'])
+        end
+        puts property.inspect
+    end
+    puts "properties are seeded"
+end
